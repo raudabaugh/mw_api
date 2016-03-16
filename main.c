b0318c874a12fa919af5474752e1d685e8668384
Sam Raudabaugh <ser266@cornell.edu>
2016-03-16 16:54:23 -0400

    Implement work creation.

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 /* my test program main.c */
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <mpi.h>
 #include <gmp.h>
 #include "mw_api.h"
 
 struct userdef_work_t {
-  mpz_t number;
-  mpz_t *factors_to_try;
+  char lb[200];
+  char ub[200];
+  char number[200];
 };
 
 struct userdef_result_t {
@@ -25,20 +28,56 @@ mw_work_t** create_work(int argc, char **argv) {
 
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
+  mw_work_t** work_arr = NULL;
+
   if (myid == 0) {
-    mpz_t number, sqrt;
+    mpz_t number, sqrt, lb, ub, step, processors, one;
+    mpz_init(sqrt);
+    mpz_init(step);
+    mpz_init(ub);
+    mpz_init_set_ui(lb, 2);
+    mpz_init_set_ui(one, 1);
+    mpz_init_set_ui(processors, sz);
+
     mpz_init_set_str(number, argv[1], 10);
     mpz_sqrt(sqrt, number);
-    printf("%s\n", mpz_get_str(NULL, 10, sqrt));
+
+    work_arr = malloc(sizeof(mw_work_t*)*sz);
+
+    mpz_sub(step, sqrt, one);
+    mpz_tdiv_q(step, step, processors);
+    mpz_add(ub, lb, step);
+    mpz_sub(ub, ub, one);
+
+    int i;
+    for (i = 0; i < sz; ++i) {
+      mw_work_t *work = malloc(sizeof(mw_work_t));
+      strcpy(work->number, mpz_get_str(NULL, 10, number));
+      strcpy(work->lb, mpz_get_str(NULL, 10, lb));
+
+      if (i == sz - 1) {
+        strcpy(work->ub, mpz_get_str(NULL, 10, sqrt));
+      } else {
+        strcpy(work->ub, mpz_get_str(NULL, 10, ub));
+      }
+
+      work_arr[i] = work;
+
+      mpz_add(lb, lb, step);
+      mpz_add(ub, ub, step);
+    }
   }
-  return NULL;
+
+  return work_arr;
 }
+
 
 int process_results(int sz, mw_result_t *res) {
   return 0;
 }
 
 mw_result_t* do_work(mw_work_t *work) {
+  printf("Received work: %s,%s,%s\n", work->number, work->lb, work->ub);
   return NULL;
 }
 
--- mw_api.c
+++ mw_api.c
@@ -1,5 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <mpi.h>
 #include "mw_api.h"
 
 void MW_Run (int argc, char **argv, struct mw_api_spec *f) {
-  f->create(argc, argv);
+
+  int sz, myid;
+
+  MPI_Comm_size(MPI_COMM_WORLD, &sz);
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+
+  mw_work_t *work = malloc(f->work_sz);
+
+  if (myid == 0) {
+    mw_work_t** work_arr = f->create(argc, argv);
+    work = work_arr[0];
+    int i;
+    for (i = 1; i < sz; ++i) {
+      MPI_Send(work_arr[i], f->work_sz, MPI_CHAR, i, 0, MPI_COMM_WORLD);
+    }
+  } else {
+      MPI_Recv(work, f->work_sz, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  }
+
+  f->compute(work);
+  MPI_Finalize();
+  exit(0);
 }
